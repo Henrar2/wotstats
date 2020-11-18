db2578654263092b4d37fa109e91f699581a850d
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-11-18 15:36:58 +0200

    commit

---
+++ WebProject/js/about.js
@@ -0,0 +1,78 @@
+//Used in Old toString() method to print content1.innerHTML = toString();
+var fname= "Kostas";
+var lname =  "Thalassinos";
+var date= "28/07/1992";
+var residence= "Patras,Greece";
+var phoneNum= "+306980930428";
+var military= "Fulfilled";
+
+//Object for userinfo **Pointless but it demonstrates I can handle objects**
+var user = [
+    Name= "Kostas Thalassinos",
+    Born= "28/07/1992",
+    Residence= "Patras,Greece",
+    Phone= "+30 6900000009",
+    military= "Fulfilled",
+];
+
+//This object is made to print the heading >> headings[i]: user[i] >> Name: Kostas Thalassinos || Born: 28/07/1992 etc
+var headings = [
+    Name= "Name: ",
+    Born= "Born: ",
+    Residence= "Residence: ",
+    Phone= "Phone: ",
+    Military= "Military Obligations: ",
+];
+
+function about(){
+
+    var i=0;
+    document.getElementById("heading").innerText = "Contact Me";
+
+    
+    //Remove all content
+    document.getElementById(`content1`).remove();
+
+    //Create Table
+    table = document.createElement(`table`);
+    table.setAttribute(`id`,`table`);
+    table.setAttribute(`class`,`table`);
+    container.appendChild(table);
+
+    for(let i=0;i<user.length;i++){
+
+        //Create Row
+        row = document.createElement(`tr`);
+        row.setAttribute(`id`,`tr${i}`);
+        row.setAttribute(`class`,`trow`);
+        document.getElementById(`table`).appendChild(row);
+
+        //Give Row a Heading first -- vertical table
+        heading = document.createElement(`th`);
+        heading.setAttribute(`id`,`th${i}`);
+        heading.setAttribute(`class`,`theading`);
+        document.getElementById(`tr${i}`).appendChild(heading);
+
+        //Give Data to Row after heading
+        data = document.createElement(`td`);
+        data.setAttribute(`id`,`td${i}`);
+        data.setAttribute(`class`,`tdata`);
+        document.getElementById(`tr${i}`).appendChild(data);
+        
+        //Each td gets 1 value from the object (name birth etc)
+        document.getElementById(`th${i}`).textContent = headings[i];
+        document.getElementById(`td${i}`).textContent = user[i];
+    }
+}
+//Old Method 
+//
+function toString(){
+    return(
+        
+        `<i class="fa fa-user" aria-hidden="true"></i>${fname} ${lname}<br>
+        <i class="fa fa-birthday-cake" aria-hidden="true"></i> ${date}<br>
+        <i class="fa fa-home" aria-hidden="true"></i> ${residence}<br>
+        <i class="fa fa-mobile" aria-hidden="true"></i> ${phoneNum}<br>
+        `
+    )
+}
